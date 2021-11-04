cb0604aeea7b1b1f68f5e20f74a8390b5aa01906
TalieD <Nataliemurphy500@gmail.com>
2021-11-04 13:55:22 -0700

    adds stopwatch to app, time seems off to me still

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,15 +1,55 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Stopwatch extends Component {
-    render() {
-        return (
-            <div className='stopwatch'>
-            <h2>Stopwatch</h2>
-            <span className='stopwatch-time'>0</span>
-            <button>Start</button>
-            <button>Restart</button>
-                
-            </div>
-        )
+  state = {
+    isRunning: false,
+    elapsedTime: 0,
+    previousTime: 0,
+  };
+
+  componentDidMount() {
+    this.intervalID = setInterval(() => this.tick(), 100);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+
+  tick = () => {
+    if (this.state.isRunning) {
+      const now = Date.now();
+      this.setState((prevState) => ({
+        previousTime: now,
+        elapsedTime: prevState.elapsedTime + (now - this.state.previousTime),
+      }));
     }
+  };
+
+  handleStopwatch = () => {
+    this.setState((prevState) => ({
+      isRunning: !prevState.isRunning,
+    }));
+    if (!this.state.isRunning) {
+      console.log("starting...");
+      this.setState({ previousTime: Date.now() });
+    }
+  };
+
+  handleReset = () => {
+    this.setState({ elapsedTime: 0 });
+  };
+
+  render() {
+    const seconds = Math.floor(this.state.elapsedTime / 1000);
+    return (
+      <div className='stopwatch'>
+        <h2>Stopwatch</h2>
+        <span className='stopwatch-time'>{seconds}</span>
+        <button onClick={this.handleStopwatch}>
+          {this.state.isRunning ? "Stop" : "Start"}
+        </button>
+        <button onClick={this.handleReset}>Restart</button>
+      </div>
+    );
+  }
 }
