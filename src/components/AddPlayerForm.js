c9e87e170d58870968003de3f4b3608b041cfb30
TalieD <Nataliemurphy500@gmail.com>
2021-11-04 21:58:01 -0700

    this adds new icon and updates addplayer form to be a stateless component.

--- src/components/AddPlayerForm.js
+++ src/components/AddPlayerForm.js
@@ -1,38 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-export default class AddPlayerForm extends Component {
+const AddPlayerForm = ({ addPlayer }) => {
+  let playerInput = React.createRef();
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    addPlayer(playerInput.current.value);
+    e.currentTarget.reset();
+  };
 
-    state = {
-        value: ''
-    }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type='text'
+        ref={playerInput}
+        placeholder="Enter a player's name"
+      />
 
-    handleValueChange = (e) => {
-        //console.log('this is e: '+ e.target.)
-        this.setState({value: e.target.value})
-    }
+      <input type='submit' value='Add Player' />
+    </form>
+  );
+};
 
-    handleSubmit = (e) =>{
-        e.preventDefault();
-        this.props.addPlayer(this.state.value);
-        this.setState({value:''})
-    }
-
-    render() {
-        console.log(this.state.value)
-        
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <input 
-                    type="text"
-                    value = {this.state.value}
-                    onChange={this.handleValueChange}
-                    placeholder="Enter a players name"
-                />
-                <input 
-                    type='submit'
-                    value ='Add Player'
-                />
-            </form>
-        )
-    }
-}
+export default AddPlayerForm;
